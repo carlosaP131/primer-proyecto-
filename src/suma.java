14b2fb0de64662a3c9f9728e1f2d48dc372bc4db
carlos <carlos.cantrax303@gmail.com>
2022-03-08 16:29:41 -0600

    quinto commit

--- src/suma.java
+++ src/suma.java
@@ -1,7 +1,7 @@
 
 public class suma {
     public static void main( String[] args ) {
-    int a = 15, b = 10, c = 0;
+    int a = 16, b = 12, c = 0;
     c = a + b;
     System. out. println( "La suma de a y b es: " + c );
     
